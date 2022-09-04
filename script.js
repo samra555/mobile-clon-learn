7e39ca1fce62542baf8645f5c2d371a19369b3fb
Smirnov Kirill <samrarus555@gmail.com>
2022-09-04 16:35:38 +0500

    cl-v3

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ window.addEventListener("DOMContentLoaded", () => {
   box.addEventListener("touchstart", (e) => {
     e.preventDefault();
     console.log("start");
+    console.log(e.targetTouches);
   });
+
+  box.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    console.log(e.targetTouches[0].pageX);
+  });
+  //
+  // box.addEventListener("touchend", (e) => {
+  //   e.preventDefault();
+  //   console.log("end");
+  // });
 });
