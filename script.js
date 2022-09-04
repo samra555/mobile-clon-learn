1b49f5a582fc38df952a561abea5eeeb4d7415eb
Smirnov Kirill <samrarus555@gmail.com>
2022-09-04 15:55:03 +0500

    v1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //cloone
+console.log(1);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelector(".box");
+
+  box.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    console.log("start");
+  });
+});
